097474d8c68f2be355b495a2e6adef9a1e0e9f62
Chubi Adama <adamachubi@gmail.com>
2019-09-04 14:51:32 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'; 
+ 
+  
+
+  ReactDOM.render(<p>hello there</p>, document.getElementById('app'));
+  
